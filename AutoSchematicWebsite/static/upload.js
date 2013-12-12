d84df5103297a28c55f29774952c2727f15cec92
JLangowitz <joshua.langowitz@students.olin.edu>
2013-12-12 18:05:41 -0500

    prettier upload button:

---
+++ AutoSchematicWebsite/static/upload.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // var position = $('#upload').position();
+    // var offset = $('#upload').offset();
+    // console.log(position);
+    // $('#cover').position(position);
+    // $('#cover').offset(offset);
+    $('#fileInput').bootstrapFileInput();
+});
